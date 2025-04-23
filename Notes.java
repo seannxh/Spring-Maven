54d22b5a43c0751c7175704dbac1cc97f02e9720
seannxh <nohsean6@gmail.com>
2025-04-23 01:23:52 -0500

    Maven Dependency

---
+++ Notes.java
@@ -0,0 +1,7 @@
+//Maven == Project Manager of Spring == Dependency Manager
+//Pom.xml is like the depency manager
+//Can alt click it to see all the depencies thats added with that perticular package
+//Maven Manager in on right side m logo to see all the depdency jar files or view -> tool window -> maven
+//After adding new depedency to go to maven like above -> click reload circle -> click rebuild all maven tools
+// ^ will download the dependecy
+//Maven transitive Dependecies
---
+++ src/main/java/com/mavenproject/maven_learn/MavenLearnApplication.java
@@ -0,0 +1,13 @@
+package com.mavenproject.maven_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenLearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenLearnApplication.class, args);
+	}
+
+}
