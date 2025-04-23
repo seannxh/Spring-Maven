9ac36321d99419985532ad8b0bc458f275f96bc7
seannxh <nohsean6@gmail.com>
2025-04-23 01:39:15 -0500

    pom.xml understanding

--- Notes.java
+++ Notes.java
@@ -5,3 +5,14 @@
 //After adding new depedency to go to maven like above -> click reload circle -> click rebuild all maven tools
 // ^ will download the dependecy
 //Maven transitive Dependecies
+
+
+//Parent POM
+//Go to maven tab and right click name of file and click show effective POM to see everything behind the back of maven
+// ^ can do this my right clicking the pom as well
+//Good for finding dependcy version
+//Dependecy management is where you define the management version of the dependency
+
+//How other users incorporate into our porject
+//GroupID is similiar to package name
+//artifactid is similiar to class
